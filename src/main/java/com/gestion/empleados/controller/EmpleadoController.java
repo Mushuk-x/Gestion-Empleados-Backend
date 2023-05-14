80e067cbf644534f3d8fa0e992135461f2b6ff3f
alantarabini <alanrtarabinii@gmail.com>
2023-05-14 14:16:57 -0300

    FindById and CreateEmp in controller

--- src/main/java/com/gestion/empleados/controller/EmpleadoController.java
+++ src/main/java/com/gestion/empleados/controller/EmpleadoController.java
@@ -2,11 +2,11 @@ package com.gestion.empleados.controller;
 
 import com.gestion.empleados.model.Empleado;
 import com.gestion.empleados.repository.EmpleadoRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/")
@@ -17,8 +17,37 @@ public class EmpleadoController {
         this.repository = repository;
     }
 
+    //Buscar todos los empleados
     @GetMapping("/empleados")
     public List<Empleado> ListaEmpleados (){
         return repository.findAll();
     }
+
+
+    // Buscar empleado mediante ID
+    @GetMapping("/empleados/{id}")
+    public ResponseEntity<Empleado> FindId(@PathVariable Long id){
+        Optional<Empleado> empleadoOpt = repository.findById(id);
+        if (empleadoOpt.isPresent()){
+            return ResponseEntity.ok(empleadoOpt.get());
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // Crear un empleado
+    @PostMapping("/empleados")
+    public ResponseEntity<Empleado> CreateEmp(@RequestBody Empleado empleado) {
+        ResponseEntity<Empleado> result;
+        if (empleado.getId() != null) {
+            result = ResponseEntity.badRequest().build();
+        } else {
+            Empleado emp = repository.save(empleado);
+            result = ResponseEntity.ok(emp);
+        }
+        return result;
+    }
+
+
+
 }
